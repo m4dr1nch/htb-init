65569c371eb633bddc036f58fcf8149fd4552b00
m4dr1nch <martins.sav@protonmail.com>
2024-10-05 15:21:39 +0300

    Changed directory structure.

--- htb-init.py
+++ htb-init.py
@@ -32,22 +32,25 @@ def create_dirs() -> None:
     create_dir('/work')
     create_dir('/work/ctf')
     create_dir('/work/ctf/htb')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/scans')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/scans/nmap')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/scans/gobuster')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/scans/ffuf')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/scans/cme')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/scans/enum4linux')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/loot')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/expl')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/sandbox')
-    create_dir(f'/work/ctf/htb/{ARGS["name"]}/www')
+    create_dir('/work/ctf/htb/Machines')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans/nmap')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans/gobuster')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans/ffuf')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans/cme')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans/nxc')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans/enum4linux')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/scans/ldapsearch')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/loot')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/expl')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/sandbox')
+    create_dir(f'/work/ctf/htb/Machines{ARGS["name"]}/www')
 
 
 def update_zshrc() -> None:
     os.system(f'sed -i "s/export IP=\'.*\'/export IP=\'{ARGS["ip"]}\'/g" ~/.zshrc')
-    os.system(f'sed -i "s/export CHD=\'.*\'/export CHD=\'\\/work\\/ctf\\/htb\\/{ARGS["name"]}\'/g" ~/.zshrc')
+    os.system(f'sed -i "s/export CHD=\'.*\'/export CHD=\'\\/work\\/ctf\\/htb\\/Machines\\/{ARGS["name"]}\'/g" ~/.zshrc')
 
 
 def main() -> None:
